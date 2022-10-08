33f837109bf24978359584e57603a40ed7252b5c
Sitaram0104 <meenasitu0104@gmail.com>
2022-10-08 19:32:27 +0530

    edit

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,78 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
 
+const CarTypes = ["Sedan", "SUV", "Van", "Magic"];
+
 export default function Home() {
+  const [pickup, setPickup] = useState("");
+  const [destination, setDestination] = useState("");
+  const [pickupTime, setPickupTime] = useState("");
+  const [pickupDate, setPickupDate] = useState("");
+  const [noofPersons, setNoofPersons] = useState(1);
+  const [carType, setCarType] = useState("Sedan");
+  const [emailId, setEmailId] = useState("");
+  const [mobile, setMobile] = useState(1);
+
   const [modalShow, setModalShow] = useState(false);
   const count = useSelector(selectValue);
   const dispatch = useDispatch();
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal
+        {...props}
+        size="sm"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            verifying OTP
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="text-center">
+            <Image src="/loading-loading-forever.gif" width={50} height={50} />
+            <Image src="/green-checkmark.png" width={50} height={50} />
+            <div className="d-flex flex-row align-items-center justify-content-center lh-sm">
+              <p className="fw-bold fst-italic m-0">Pickup: </p>
+              <p className="text-muted ms-1 m-0">{pickup}</p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">Destination: </p>
+              <p className="text-muted ms-1 m-0">{destination}</p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">pickupTime: </p>
+              <p className="text-muted ms-1 m-0">
+                {pickupTime}
+                {pickupDate}
+              </p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">number of Persons: </p>
+              <p className="text-muted ms-1 m-0">{noofPersons}</p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">Car Type: </p>
+              <p className="text-muted ms-1 m-0">{carType}</p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">Email Id: </p>
+              <p className="text-muted ms-1 m-0">{emailId}</p>
+            </div>
+            <div className="d-flex flex-row align-items-center justify-content-center">
+              <p className="fw-bold fst-italic m-0">Mobile Number: </p>
+              <p className="text-muted ms-1 m-0">{mobile}</p>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Edit Booking</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
 
   return (
     <div className="min-vh-100 d-flex align-items-center justify-content-center flex-column">
@@ -40,6 +108,7 @@ export default function Home() {
               width={70}
               height={40}
               alt="logo"
+              style={{ background: "black" }}
             />
             <h1 className="text-white">Welcome to Carbuk</h1>
           </div>
@@ -47,21 +116,25 @@ export default function Home() {
             <div className="mb-3 d-flex flex-row">
               <div className="form-floating mb-3 w-50">
                 <input
-                  type="search"
+                  type="text"
                   className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
+                  id="pickupInput"
+                  placeholder="Kharagpur"
+                  value={pickup}
+                  onChange={(e) => setPickup(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Enter Pickup Location</label>
+                <label htmlFor="pickupInput">Enter Pickup Location</label>
               </div>
               <div className="form-floating mb-3 w-50">
                 <input
-                  type="search"
+                  type="text"
                   className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
+                  id="destinationInput"
+                  placeholder="Kolkata"
+                  value={destination}
+                  onChange={(e) => setDestination(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Enter Destination</label>
+                <label htmlFor="destinationInput">Enter Destination</label>
               </div>
             </div>
             <div className="mb-3 d-flex flex-row">
@@ -69,21 +142,23 @@ export default function Home() {
                 <input
                   type="time"
                   className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                  value="13:21"
+                  id="pickupTimeInput"
+                  placeholder="13:21"
+                  value={pickupTime}
+                  onChange={(e) => setPickupTime(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Select Pickup Time</label>
+                <label htmlFor="pickupTimeInput">Select Pickup Time</label>
               </div>
               <div className="form-floating w-50">
                 <input
                   type="date"
                   className="form-control"
-                  id="floatingInput"
-                  placeholder="name@example.com"
-                  value="2022-09-08"
+                  id="pickupDateInput"
+                  placeholder="2022-01-01"
+                  value={pickupDate}
+                  onChange={(e) => setPickupDate(e.target.value)}
                 />
-                <label htmlFor="floatingInput">Select Pickup Date</label>
+                <label htmlFor="pickupDateInput">Select Pickup Date</label>
               </div>
             </div>
             <div className="mb-3">
@@ -96,6 +171,7 @@ export default function Home() {
               <button
                 type="button"
                 className="btn btn-secondary btn-sm ms-4 px-3"
+                onClick={() => setNoofPersons(noofPersons - 1)}
               >
                 -
               </button>
@@ -103,40 +179,35 @@ export default function Home() {
                 type="button"
                 className="btn btn-primary btn-sm px-4 mx-1"
               >
-                1
+                {noofPersons}
               </button>
-              <button type="button" className="btn btn-secondary btn-sm px-3">
+              <button
+                type="button"
+                className="btn btn-secondary btn-sm px-3"
+                onClick={() => setNoofPersons(noofPersons + 1)}
+              >
                 +
               </button>
             </div>
             <div className="mb-3">
               <label
                 htmlFor="exampleInputPassword1"
-                className="form-label text-white"
+                className="form-label text-white me-2"
               >
                 Car Type
               </label>
-              <button
-                type="button"
-                className="btn btn-secondary btn-sm ms-2 px-4"
-              >
-                Sedan
-              </button>
-              <button
-                type="button"
-                className="btn btn-primary btn-sm px-4 mx-1"
-              >
-                SUV
-              </button>
-              <button type="button" className="btn btn-secondary btn-sm px-4">
-                Van
-              </button>
-              <button
-                type="button"
-                className="btn btn-secondary btn-sm px-4  mx-1"
-              >
-                Magic
-              </button>
+              {CarTypes.map((carName, index) => (
+                <button
+                  type="button"
+                  className={`btn btn-sm ms-1 px-3 ${
+                    carName === carType ? "btn-primary" : "btn-secondary"
+                  }`}
+                  key={index}
+                  onClick={() => setCarType(carName)}
+                >
+                  {carName}
+                </button>
+              ))}
             </div>
             <div className="form-floating mb-3">
               <input
@@ -171,9 +242,13 @@ export default function Home() {
               </div>
             </div>
             <Button
+              type="submit"
               className="w-100"
               variant="primary"
-              onClick={() => setModalShow(true)}
+              onClick={(e) => {
+                e.preventDefault();
+                setModalShow(true);
+              }}
             >
               verify OTP
             </Button>
@@ -181,31 +256,5 @@ export default function Home() {
         </div>
       </main>
     </div>
-  );
-}
-
-function MyVerticallyCenteredModal(props) {
-  return (
-    <Modal
-      {...props}
-      size="sm"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          verifying OTP
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="text-center">
-          <Image src="/loading-loading-forever.gif" width={50} height={50} />
-          <Image src="/green-checkmark.png" width={50} height={50} />
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button onClick={props.onHide}>Close</Button>
-      </Modal.Footer>
-    </Modal>
   );
 }
